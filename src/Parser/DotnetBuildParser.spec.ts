18c87bf9aff43ceb4f7c6a81151d39930ae85d8e
Plai <plai.rt.w@gmail.com>
2020-12-09 15:46:49 +0700

    Simplify dotnet build parser

--- src/Parser/DotnetBuildParser.spec.ts
+++ src/Parser/DotnetBuildParser.spec.ts
@@ -6,24 +6,13 @@ describe('DotnetBuildParser tests', () => {
   const cwdWin = 'C:\\source';
   const cwdUnix = '/dir';
 
-  const contentWithLineOffset =
-    '1:7>C:\\source\\Broken.cs(6,8): warning AG0030: Prevent use of dynamic [C:\\source\\Broken.csproj]';
+  const logWithSource = `1:7>C:\\source\\Broken.cs(6,8): warning AG0030: Prevent use of dynamic [C:\\source\\Broken.csproj]`;
+  const logWithNoSource = `1:7>CSC : error CS5001: Program does not contain a static 'Main' method suitable for an entry point [C:\\source\\Broken.csproj]`;
+  const logWithUnrelatedSource = `9:8>/usr/share/dotnet/sdk/3.1.402/Microsoft.Common.CurrentVersion.targets(2084,5): warning MSB3277: some message [/dir/Tests/project.csproj]`;
+  const contentWithNoPathAtTheEnd = `13:11>/dir/File.csproj : warning NU1701: This package may not be fully compatible with your project.`;
 
-  const contentNoLineOffset =
-    "1:7>CSC : error CS5001: Program does not contain a static 'Main' method suitable for an entry point [C:\\source\\Broken.csproj]";
-
-  const contentWithNotValid = `
-  3:9>/dir/Tests/File.cs(17,43): warning CS0649: Field 'SearchResultMockBuilder._urgencyScore' is never assigned to, and will always have its default value null [/dir/Tests/project.csproj]
-  17:6>/dir/Tests/File2.cs(226,17): warning CS0219: The variable 'langId' is assigned but its value is never used [/dir/Tests/project.csproj]
-    9:8>/usr/share/dotnet/sdk/3.1.402/Microsoft.Common.CurrentVersion.targets(2084,5): warning MSB3277: Found conflicts between different versions of "Microsoft.Extensions.Configuration.Json" that could not be resolved.  These reference conflicts are listed in the build log when log verbosity is set to detailed. [/dir/Tests/project.csproj]`;
-
-  const contentWithNoPathAtTheEnd =
-    '13:11>/dir/File.csproj : warning NU1701: This package may not be fully compatible with your project.';
-
-  it('Should parse correctly when (line, offset) is provided', () => {
-    const result = new DotnetBuildParser(cwdWin)
-      .withContent(contentWithLineOffset)
-      .getLogs();
+  it('Should parse log with source path correctly', () => {
+    const result = new DotnetBuildParser(cwdWin).withContent(logWithSource).getLogs();
     expect(result).toHaveLength(1);
     expect(result[0]).toEqual({
       source: `Broken.cs`,
@@ -31,44 +20,48 @@ describe('DotnetBuildParser tests', () => {
       line: 6,
       lineOffset: 8,
       msg: `AG0030: Prevent use of dynamic`,
-      log: contentWithLineOffset,
+      log: logWithSource,
       valid: true,
     } as LogType);
   });
 
-  it('Should parse correctly when (line, offset) is not provided', () => {
-    const result = new DotnetBuildParser(cwdWin)
-      .withContent(contentNoLineOffset)
-      .getLogs();
+  it('Should parse log without source path correctly and flag as invalid and use csproj as source', () => {
+    const result = new DotnetBuildParser(cwdWin).withContent(logWithNoSource).getLogs();
     expect(result).toHaveLength(1);
     expect(result[0]).toEqual({
       source: `Broken.csproj`,
       severity: LogSeverity.error,
-      line: undefined,
-      lineOffset: undefined,
+      line: NaN,
+      lineOffset: NaN,
       msg: `CS5001: Program does not contain a static 'Main' method suitable for an entry point`,
-      log: contentNoLineOffset,
-      valid: true,
+      log: logWithNoSource,
+      valid: false,
+    } as LogType);
+  });
+
+  it('Should parse log unrelated source path correctly and flag as invalid and use csproj as source', () => {
+    const result = new DotnetBuildParser(cwdUnix)
+      .withContent(logWithUnrelatedSource)
+      .getLogs();
+    expect(result).toHaveLength(1);
+    expect(result[0]).toEqual({
+      source: `project.csproj`,
+      severity: LogSeverity.warning,
+      line: 2084,
+      lineOffset: 5,
+      msg: `MSB3277: some message`,
+      log: logWithUnrelatedSource,
+      valid: false,
     } as LogType);
   });
 
   it('Should be able to call `withContent` multiple times and add all content together', () => {
     const result = new DotnetBuildParser(cwdWin)
-      .withContent(contentWithLineOffset)
-      .withContent(contentNoLineOffset)
+      .withContent(logWithSource)
+      .withContent(logWithNoSource)
       .getLogs();
 
     expect(result).toHaveLength(2);
-  });
-
-  it('Should parse with valid/invalid correctly', () => {
-    const result = new DotnetBuildParser(cwdUnix)
-      .withContent(contentWithNotValid)
-      .getLogs();
-    const valid = result.filter((el) => el.valid);
-    const invalid = result.filter((el) => !el.valid);
-    expect(valid).toHaveLength(2);
-    expect(invalid).toHaveLength(1);
   });
 
   it('Should do nothing if put empty string', () => {
