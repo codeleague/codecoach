8c3d1e99ce8963ee96ae0c5f6968fb7947001f21
Plai <plai.rt.w@gmail.com>
2021-03-09 01:30:49 +0700

    Add comment group mapper

--- src/Provider/GitHub/GitHub.ts
+++ src/Provider/GitHub/GitHub.ts
@@ -3,6 +3,11 @@ import { Log } from '../../Logger';
 import { LogSeverity, LogType } from '../../Parser';
 import { onlyIn, onlySeverity } from '../utils/filter.util';
 import { MessageUtil } from '../utils/message.util';
+import {
+  CommentFileStructure,
+  CommentStructure,
+  Comment,
+} from '../@interfaces/CommentTypes';
 import { CommitStatus } from './CommitStatus';
 import { IGitHubPRService } from './IGitHubPRService';
 
@@ -23,18 +28,27 @@ export class GitHub implements VCS {
       Log.debug(`Commit SHA ${commitId}`);
       Log.debug('Touched files', touchedFiles);
 
+      const comments = this.groupComments(touchedFileLog);
+
       const reviewResults = await Promise.all(
-        touchedFileLog.map((log) => this.toCreateReviewComment(commitId, log)),
+        comments.map((comment) => this.toCreateReviewComment(commitId, comment)),
       );
-      const reviewedLogs = reviewResults.filter((log) => log);
+      const reviewedComments = reviewResults.filter((log): log is Comment => !!log);
 
       Log.info(
-        `Create ${reviewedLogs.length} review comments. (with ${
-          reviewResults.length - reviewedLogs.length
+        `Create ${reviewedComments.length} review comments. (with ${
+          comments.length - reviewedComments.length
         } failed)`,
       );
-      const nOfErrors = reviewedLogs.filter(onlySeverity(LogSeverity.error)).length;
-      const nOfWarnings = reviewedLogs.filter(onlySeverity(LogSeverity.warning)).length;
+      const nOfErrors = reviewedComments.reduce(
+        (sum, comment) => sum + comment.errors,
+        0,
+      );
+
+      const nOfWarnings = reviewedComments.reduce(
+        (sum, comment) => sum + comment.warnings,
+        0,
+      );
 
       if (nOfWarnings + nOfErrors > 0 || invalidLogs.length > 0) {
         const overview = MessageUtil.generateOverviewMessage(nOfErrors, nOfWarnings);
@@ -73,21 +87,20 @@ ${issuesTableContent}
 
   private toCreateReviewComment = async (
     commitSha: string,
-    log: LogType,
-  ): Promise<LogType | null> => {
+    comment: Comment,
+  ): Promise<Comment | null> => {
+    const { text, file, line } = comment;
     try {
-      await this.prService.createReviewComment(
-        commitSha,
-        MessageUtil.createMessageWithEmoji(log.msg, log.severity),
-        log.source,
-        log.line,
-      );
-      Log.debug('GitHub create review success', { log });
-      return log;
+      await this.prService.createReviewComment(commitSha, text, file, line);
+      Log.debug('GitHub create review success', { text, file, line });
+      return comment;
     } catch (e) {
       // todo: this is workaround; handle comment on restrict zone in github
       const { name, status } = e ?? {};
-      Log.warn('GitHub create review failed', { log, error: { name, status } });
+      Log.warn('GitHub create review failed', {
+        comment,
+        error: { name, status },
+      });
       return null;
     }
   };
@@ -110,5 +123,34 @@ ${issuesTableContent}
 
     await Promise.all([...deleteComments, ...deleteReviews]);
     Log.debug('Delete CodeCoach comments completed');
+  }
+
+  private groupComments(logs: LogType[]): Comment[] {
+    const commentMap = logs.reduce((map: CommentStructure, log) => {
+      const { source: file, line, severity, msg } = log;
+      const text = MessageUtil.createMessageWithEmoji(msg, severity);
+
+      if (!line) return map;
+
+      const currentWarnings = map?.[file]?.[line]?.warnings ?? 0;
+      const currentErrors = map?.[file]?.[line]?.errors ?? 0;
+      const currentText = map?.[file]?.[line]?.text ?? '';
+
+      const nextObject: Comment = {
+        text: `${currentText}\n${text}`,
+        errors: currentErrors + (severity === LogSeverity.error ? 1 : 0),
+        warnings: currentWarnings + (severity === LogSeverity.warning ? 1 : 0),
+        file,
+        line,
+      };
+
+      const fileCommentUpdater: CommentFileStructure = { [line]: nextObject };
+      const updatedFileComment = Object.assign({}, map?.[file], fileCommentUpdater);
+
+      const mapUpdater: CommentStructure = { [file]: updatedFileComment };
+      return Object.assign({}, map, mapUpdater);
+    }, {});
+
+    return Object.values(commentMap).flatMap((file) => Object.values(file));
   }
 }
