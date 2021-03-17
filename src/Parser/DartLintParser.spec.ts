3a566f5a95fe9364ab545d073196a3d2075e429e
Jacek Kozal <jacek.kozal@agoda.com>
2021-03-17 11:39:06 +0700

    additional test for broken lines of dart lint

--- src/Parser/DartLintParser.spec.ts
+++ src/Parser/DartLintParser.spec.ts
@@ -11,7 +11,13 @@ describe('DartLintStyleParser', () => {
     error • Sort child properties last in widget instance creations • lib/presentation/widgets/platform_flat_button.dart:34:9 • sort_child_properties_last
     error • The annotation 'Timeout' can only be used on libraries • test_driver/tests/offline/offline_test.dart:13:2 • invalid_annotation_target
   `;
-  const mockedContentWithNoError = `
+  const mockedContentWithBrokenLines = `
+    The plugin can be updated to the v2 Android Plugin APIs by following https://flutter.dev/go/android-plugin-migration.
+    Analyzing host_app...                                           
+    info • Unused import: 'dart:async' • api/modules/host_manage_api/lib/auth/auth.dart • unused_import
+    lib/domain/providers/sharable_images_repo.dart:114:5 • await_only_futures
+  `;
+  const mockedContentWithoutResults = `
     The plugin can be updated to the v2 Android Plugin APIs by following https://flutter.dev/go/android-plugin-migration.
     Analyzing host_app...
   `;
@@ -61,6 +67,21 @@ describe('DartLintStyleParser', () => {
     });
   });
 
+  it('Should parse content with broken lines correctly', () => {
+    const result = new DartLintParser(cwd).parse(mockedContentWithBrokenLines);
+    expect(result).toHaveLength(1);
+
+    expect(result[0]).toEqual({
+      source: `api/modules/host_manage_api/lib/auth/auth.dart`,
+      severity: LogSeverity.info,
+      line: NaN,
+      lineOffset: NaN,
+      msg: `Unused import: 'dart:async'`,
+      log: `unused_import`,
+      valid: true,
+    });
+  });
+
   it('Should do nothing if put empty string', () => {
     expect(() => new DartLintParser(cwd).parse('')).toHaveLength(0);
   });
@@ -69,7 +90,8 @@ describe('DartLintStyleParser', () => {
     expect(() => new DartLintParser(cwd).parse(':')).toHaveLength(0);
   });
 
-  it('Should parse content with no error correctly', () => {
-    expect(() => new DartLintParser(cwd).parse(mockedContentWithNoError)).toHaveLength(0);
+  it('Should parse content with no results correctly', () => {
+    const result = new DartLintParser(cwd).parse(mockedContentWithoutResults);
+    expect(result).toHaveLength(0);
   });
 });
