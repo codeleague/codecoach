fdf109108e917d558b33c181d30b148692b94265
yee2542 <yee2542gtl@gmail.com>
2021-04-22 04:24:32 +0700

    wip config test

--- src/Config/Config.spec.ts
+++ src/Config/Config.spec.ts
@@ -11,13 +11,19 @@ const MOCK_ARGS = [
   '-o=./tmp/out.json',
 ];
 
+const MOCK_ARGS_W_CONFIG_YAML = [
+  '/usr/local/Cellar/node/15.13.0/bin/node',
+  '/Users/codecoach/src/app.ts',
+  '--config=sample/config/config.yml',
+];
+
 export const EXPECTED_MOCK_ARGS = [
   '/usr/local/Cellar/node/15.13.0/bin/node',
   '/Users/codecoach/src/app.ts',
   'https://github.com/codeleague/codecoach.git',
   'removeOldComment',
   'placeyourtokenhere',
-  '15',
+  15,
   'dotnetbuild;./sample/dotnetbuild/build.content;/repo/src',
   './tmp/out.json',
 ];
@@ -25,12 +31,19 @@ export const EXPECTED_MOCK_ARGS = [
 describe('Config Test', () => {
   let config: typeof Config;
 
-  beforeEach(async () => {
-    process.argv = MOCK_ARGS;
+  beforeEach(() => {
+    jest.resetModules();
   });
 
   it('Should able to parse this args correctly', async () => {
+    process.argv = MOCK_ARGS;
     config = (await import('./Config')).Config;
     expect(config.provider.repoUrl).toBe(EXPECTED_MOCK_ARGS[2]);
+  });
+
+  it('Should able to use a config file without passing other args', async () => {
+    process.argv = MOCK_ARGS_W_CONFIG_YAML;
+    config = (await import('./Config')).Config;
+    console.log(config);
   });
 });
