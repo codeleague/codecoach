348a57b05f3fc1c33fd8705a46f39b191bce38bb
yee2542 <yee2542gtl@gmail.com>
2021-04-22 04:05:19 +0700

    refactor Config.spec.ts

--- src/Config/Config.spec.ts
+++ src/Config/Config.spec.ts
@@ -1,9 +1,36 @@
 import { Config } from './Config';
-import { EXPECTED_MOCK_ARGS } from '../../jest.setup';
+
+const MOCK_ARGS = [
+  '/usr/local/Cellar/node/15.13.0/bin/node',
+  '/Users/codecoach/src/app.ts',
+  '--url=https://github.com/codeleague/codecoach.git',
+  '--removeOldComment',
+  '--token=placeyourtokenhere',
+  '--pr=15',
+  '-f=dotnetbuild;./sample/dotnetbuild/build.content;/repo/src',
+  '-o=./tmp/out.json',
+];
+
+export const EXPECTED_MOCK_ARGS = [
+  '/usr/local/Cellar/node/15.13.0/bin/node',
+  '/Users/codecoach/src/app.ts',
+  'https://github.com/codeleague/codecoach.git',
+  'removeOldComment',
+  'placeyourtokenhere',
+  '15',
+  'dotnetbuild;./sample/dotnetbuild/build.content;/repo/src',
+  './tmp/out.json',
+];
 
 describe('Config Test', () => {
-  it('Should able to parse this args correctly', () => {
-    const args = Config;
-    expect(args.provider.repoUrl).toBe(EXPECTED_MOCK_ARGS[2]);
+  let config: typeof Config;
+
+  beforeEach(async () => {
+    process.argv = MOCK_ARGS;
+  });
+
+  it('Should able to parse this args correctly', async () => {
+    config = (await import('./Config')).Config;
+    expect(config.provider.repoUrl).toBe(EXPECTED_MOCK_ARGS[2]);
   });
 });
