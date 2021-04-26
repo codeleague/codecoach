adcc1fd74db3b601c3afa016c8fe5a217e6c6d05
yee2542 <yee2542gtl@gmail.com>
2021-04-26 22:54:52 +0700

    fix bad test

--- src/Config/YML.spec.ts
+++ src/Config/YML.spec.ts
@@ -3,7 +3,7 @@ import { YML } from './YML';
 describe('YML Test', () => {
   it('Should able to validate yaml file correctly', async () => {
     const config = await YML.parse('sample/config/config.yaml');
-    expect(config.output).toBe('/path/to/codecoach/output.json');
+    expect(config.output).toBe('./output.json');
     expect(config.provider.pr).toBe(40);
     expect(config.provider.removeOldComment).toBe(false);
   });
