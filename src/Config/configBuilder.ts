9d44e3b4a89f76c5c3d205d0cd49e7b63f180a8d
yee2542 <yee2542gtl@gmail.com>
2021-04-26 21:43:55 +0700

    allow args to override config file

--- src/Config/configBuilder.ts
+++ src/Config/configBuilder.ts
@@ -1,10 +1,10 @@
 import { YML } from '.';
 import { AppConfig, ConfigArgument, ProviderConfig } from './@types';
-import { ConfigYAML } from './@types/configYAML';
 import { TIME_ZONE, USER_AGENT } from './constants/defaults';
 
 const buildYMLConfig = async (args: ConfigArgument) => {
-  const parsed = await YML.parse<ConfigYAML>(args.config);
+  if (!args.config) return;
+  const parsed = await YML.parse(args.config);
   return parsed;
 };
 
@@ -14,19 +14,19 @@ export const buildProviderConfig = async (
   const configFile = await buildYMLConfig(arg);
 
   return {
-    token: arg.token || configFile.provider.token,
-    repoUrl: arg.url || configFile.provider.url,
-    prId: arg.pr || configFile.provider.pr,
-    removeOldComment: arg.removeOldComment || configFile.provider.removeOldComment,
-    userAgent: configFile.provider.userAgent || USER_AGENT,
-    timeZone: configFile.provider.timeZone || TIME_ZONE,
+    token: configFile?.provider.token || arg.token,
+    repoUrl: configFile?.provider.url || arg.url,
+    prId: configFile?.provider.pr || arg.pr,
+    removeOldComment: configFile?.provider.removeOldComment || arg.removeOldComment,
+    userAgent: configFile?.provider.userAgent || USER_AGENT,
+    timeZone: configFile?.provider.timeZone || TIME_ZONE,
   };
 };
 
 export const buildAppConfig = async (arg: ConfigArgument): Promise<AppConfig> => {
   const configFile = await buildYMLConfig(arg);
   return {
-    logFilePath: arg.output || configFile.output,
-    buildLogFiles: arg.buildLogFile || configFile.buildLogFiles,
+    logFilePath: configFile?.output || arg.output,
+    buildLogFiles: configFile?.buildLogFiles || arg.buildLogFile,
   };
 };
