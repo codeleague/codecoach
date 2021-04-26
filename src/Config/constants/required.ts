7dfa3c4f20b2f44332df4c8563212a1a09b49d31
yee2542 <yee2542gtl@gmail.com>
2021-04-26 21:44:26 +0700

    add requirement fields

--- src/Config/constants/required.ts
+++ src/Config/constants/required.ts
@@ -1,5 +1,15 @@
 import { ConfigArgument } from '..';
+import { ConfigYAML } from '../@types/configYAML';
 
 type RequiredArgs = (keyof ConfigArgument)[];
-
 export const REQUIRED_ARGS: RequiredArgs = ['url', 'pr', 'buildLogFile', 'token'];
+
+type RequiredYamlArgs = (keyof ConfigYAML)[];
+export const REQUIRED_YAML_ARGS: RequiredYamlArgs = ['provider', 'buildLogFiles'];
+
+type RequiredYamlProviderArgs = (keyof ConfigYAML['provider'])[];
+export const REQUIRED_YAML_PROVIDER_ARGS: RequiredYamlProviderArgs = [
+  'url',
+  'pr',
+  'token',
+];
