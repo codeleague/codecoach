6f82e436e6089e443b1fd861487dc58f50189ef9
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2023-06-29 17:30:44 +0700

    Make groupComments function easier to understand

--- src/AnalyzerBot/utils/commentUtil.ts
+++ src/AnalyzerBot/utils/commentUtil.ts
@@ -1,32 +1,69 @@
 import { LogSeverity, LogType } from '../../Parser';
-import { Comment, CommentFileStructure, CommentStructure } from '../@types/CommentTypes';
+import { Comment, CommentStructure } from '../@types/CommentTypes';
 import { MessageUtil } from './message.util';
 
 export function groupComments(logs: LogType[]): Comment[] {
-  const commentMap = logs.reduce((map: CommentStructure, log) => {
-    const { source: file, line, severity, msg } = log;
-    const text = MessageUtil.createMessageWithEmoji(msg, severity);
+  const commentMap = logs.reduce((state: CommentStructure, log) => {
+    const { source: file, line } = log;
 
-    if (!line) return map;
+    if (!line) return state;
 
-    const currentWarnings = map?.[file]?.[line]?.warnings ?? 0;
-    const currentErrors = map?.[file]?.[line]?.errors ?? 0;
-    const currentText = map?.[file]?.[line]?.text ?? '';
-
-    const nextObject: Comment = {
-      text: `${currentText}${text}  \n`,
-      errors: currentErrors + (severity === LogSeverity.error ? 1 : 0),
-      warnings: currentWarnings + (severity === LogSeverity.warning ? 1 : 0),
-      file,
-      line,
-    };
-
-    const fileCommentUpdater: CommentFileStructure = { [line]: nextObject };
-    const updatedFileComment = Object.assign({}, map?.[file], fileCommentUpdater);
-
-    const mapUpdater: CommentStructure = { [file]: updatedFileComment };
-    return Object.assign({}, map, mapUpdater);
+    const currentComment = getOrInitComment(state, file, line);
+    const updatedComment = updateComment(currentComment, log);
+    return updateCommentStructure(state, updatedComment);
   }, {});
 
   return Object.values(commentMap).flatMap((file) => Object.values(file));
+}
+
+function getOrInitComment(map: CommentStructure, file: string, line: number): Comment {
+  return (
+    map?.[file]?.[line] ?? {
+      text: '',
+      errors: 0,
+      warnings: 0,
+      file,
+      line,
+    }
+  );
+}
+
+function buildText(currentComment: Comment, log: LogType): string {
+  const { severity, msg } = log;
+  const { text: currentText } = currentComment;
+  const text = MessageUtil.createMessageWithEmoji(msg, severity);
+  return `${currentText}${text}  \n`;
+}
+
+function calculateErrors(currentComment: Comment, log: LogType): number {
+  const { severity } = log;
+  return currentComment.errors + (severity === LogSeverity.error ? 1 : 0);
+}
+
+function calculateWarnings(currentComment: Comment, log: LogType): number {
+  const { severity } = log;
+  return currentComment.warnings + (severity === LogSeverity.warning ? 1 : 0);
+}
+
+function updateComment(currentComment: Comment, log: LogType): Comment {
+  return {
+    text: buildText(currentComment, log),
+    errors: calculateErrors(currentComment, log),
+    warnings: calculateWarnings(currentComment, log),
+    file: currentComment.file,
+    line: currentComment.line,
+  };
+}
+
+function updateCommentStructure(
+  currentStructure: CommentStructure,
+  updatedComment: Comment,
+): CommentStructure {
+  return {
+    ...currentStructure,
+    [updatedComment.file]: {
+      ...currentStructure?.[updatedComment.file],
+      [updatedComment.line]: updatedComment,
+    },
+  };
 }
