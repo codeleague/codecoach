60ca03d52b901b1a30769b17472c9c6d442953a2
jeeyo <natchanon.nuntanirund@agoda.com>
2023-07-31 15:50:13 +0700

    fix jscpd issues not reported

--- src/Parser/JscpdParser.ts
+++ src/Parser/JscpdParser.ts
@@ -13,26 +13,37 @@ export class JscpdParser extends Parser {
       if (!content) return [];
 
       const logsJson = JSON.parse(content) as JscpdLog;
-      return logsJson.duplicates.map((el) => this.toLog(el));
+      return logsJson.duplicates.flatMap((el) => this.toLog(el));
     } catch (err) {
       Log.warn('jscpd Parser: parse with content via JSON error', content);
       throw err;
     }
   }
 
-  private toLog(log: JscpdLog['duplicates'][number]): LogType {
-    const parsed: LogType = {
-      ruleId: this.ruleId,
-      log: JSON.stringify(log),
-      line: log.secondFile.startLoc.line,
-      lineOffset: log.secondFile.startLoc.column ?? 0,
-      msg: `Found code duplication from "${log.firstFile.name}"`,
-      source: log.secondFile.name,
-      severity: LogSeverity.info,
-      valid: true,
-      type: ProjectType.jscpd,
-    };
-
-    return parsed;
+  private toLog(log: JscpdLog['duplicates'][number]): LogType[] {
+    return [
+      {
+        ruleId: this.ruleId,
+        log: JSON.stringify(log),
+        line: log.secondFile.startLoc.line,
+        lineOffset: log.secondFile.startLoc.column ?? 0,
+        msg: `Found code duplication from "${log.firstFile.name}:${log.firstFile.startLoc.line}"`,
+        source: log.secondFile.name,
+        severity: LogSeverity.warning,
+        valid: true,
+        type: ProjectType.jscpd,
+      },
+      {
+        ruleId: this.ruleId,
+        log: JSON.stringify(log),
+        line: log.firstFile.startLoc.line,
+        lineOffset: log.firstFile.startLoc.column ?? 0,
+        msg: `Found code duplication from "${log.secondFile.name}:${log.secondFile.startLoc.line}"`,
+        source: log.firstFile.name,
+        severity: LogSeverity.warning,
+        valid: true,
+        type: ProjectType.jscpd,
+      },
+    ];
   }
 }
