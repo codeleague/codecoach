6e08c3fb5f47d7504009d90559527d3671f308ca
jeeyo <natchanon.nuntanirund@agoda.com>
2023-08-08 14:10:19 +0700

    remove local debugging

--- src/Logger/Logger.ts
+++ src/Logger/Logger.ts
@@ -3,7 +3,7 @@ import { createLogger, format, transports } from 'winston';
 const { combine, prettyPrint, timestamp, simple } = format;
 
 export const Log = createLogger({
-  // silent: process.env['NODE_ENV'] === 'test', // set by jest
+  silent: process.env['NODE_ENV'] === 'test', // set by jest
   transports: [
     new transports.Console({ level: 'info', format: simple() }),
     new transports.File({
