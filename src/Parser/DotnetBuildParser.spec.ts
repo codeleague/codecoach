05a9e8952448f30f89f81439739d29bd446c907c
Plai <plai.rt.w@gmail.com>
2023-11-09 17:31:50 +0700

    Fix dotnet build parser to recognize path with space (#161)
    Co-authored-by: pdujtipiya <patipan.dujtipiya@agoda.com>

--- src/Parser/DotnetBuildParser.spec.ts
+++ src/Parser/DotnetBuildParser.spec.ts
@@ -7,6 +7,7 @@ describe('DotnetBuildParser tests', () => {
   const cwdUnix = '/dir';
 
   const logWithSource = `1:7>C:\\source\\Broken.cs(6,8): warning AG0030: Prevent use of dynamic [C:\\source\\Broken.csproj]`;
+  const logWithSourceWithSpace = `1:7>C:\\source\\some dir\\Broken.cs(6,8): warning AG0030: Prevent use of dynamic [C:\\source\\some dir\\Broken.csproj]`;
   const logWithNoSource = `1:7>CSC : error CS5001: Program does not contain a static 'Main' method suitable for an entry point [C:\\source\\Broken.csproj]`;
   const logWithUnrelatedSource = `9:8>/usr/share/dotnet/sdk/3.1.402/Microsoft.Common.CurrentVersion.targets(2084,5): warning MSB3277: some message [/dir/Tests/project.csproj]`;
   const contentWithNoPathAtTheEnd = `13:11>/dir/File.csproj : warning NU1701: This package may not be fully compatible with your project.`;
@@ -77,5 +78,12 @@ describe('DotnetBuildParser tests', () => {
 
   it('should ignore multiline-part messages', () => {
     expect(() => new DotnetBuildParser(cwdUnix).parse(multilinePart)).not.toThrowError();
+  });
+
+  it('should handle path with space', () => {
+    const items = new DotnetBuildParser(cwdWin).parse(logWithSourceWithSpace);
+    const result = items[0];
+
+    expect(result.source).toBe('some dir/Broken.cs');
   });
 });
